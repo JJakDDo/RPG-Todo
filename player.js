74a15d22f6d0a359f892f786b303a8b13161d42a
JJakDDo <kangdw124@gmail.com>
2022-06-13 21:02:51 +0900

    feat: add dark mode theme

--- player.js
+++ player.js
@@ -1,86 +1,88 @@
-export class Player{
-    constructor(){        
-        const player = JSON.parse(localStorage.getItem("player"));
-        if(player){
-            this.lvl = +player.lvl;
-            this.exp = +player.exp;
-            this.requiredExp = +player.requiredExp;
-        } else {
-            this.lvl = 1;
-            this.exp = 0;
-            this.requiredExp = 500;
-        }
+export class Player {
+  constructor() {
+    const player = JSON.parse(localStorage.getItem("player"));
+    if (player) {
+      this.lvl = +player.lvl;
+      this.exp = +player.exp;
+      this.requiredExp = +player.requiredExp;
+    } else {
+      this.lvl = 1;
+      this.exp = 0;
+      this.requiredExp = 500;
+    }
 
+    this.img = new Image();
+    this.img.src = "src/4.png";
+    this.img.onload = () => {
+      this.loaded();
+    };
+    this.cur = 0;
+    this.isLoaded = false;
 
-        this.img = new Image();
-        this.img.src = 'src/Pink_Monster_Idle_4.png';
-        this.img.onload = () => {
-            this.loaded();
-        }
-        this.cur = 0;
-        this.isLoaded = false;
+    this.totalFrame = 8;
+    this.curFrame = 0;
 
-        this.totalFrame = 4;
+    this.imgWidth = 48;
+    this.imgHeight = 96;
+
+    this.fps = 8;
+    this.fpsTime = 1000 / this.fps;
+  }
+
+  addExp(exp) {
+    this.exp += exp;
+    if (this.exp >= this.requiredExp) {
+      this.lvl++;
+      this.exp -= this.requiredExp;
+      this.requiredExp += 200;
+    }
+    this.savePlayer();
+  }
+
+  savePlayer() {
+    localStorage.setItem(
+      "player",
+      JSON.stringify({
+        lvl: this.lvl,
+        exp: this.exp,
+        requiredExp: this.requiredExp,
+      })
+    );
+  }
+
+  loaded() {
+    this.isLoaded = true;
+  }
+
+  draw(ctx, t) {
+    if (!this.time) {
+      this.time = t;
+    }
+
+    const now = t - this.time;
+    if (now > this.fpsTime) {
+      this.time = t;
+      this.curFrame += 1;
+      if (this.curFrame == this.totalFrame) {
         this.curFrame = 0;
-
-        this.imgWidth = 32;
-        this.imgHeight = 32;
-
-        this.fps = 8;
-        this.fpsTime = 1000 / this.fps;
+      }
     }
-
-    addExp(exp){
-        this.exp += exp;
-        if(this.exp >= this.requiredExp){
-            this.lvl++;
-            this.exp -= this.requiredExp;
-            this.requiredExp += 200;
-        }
-        this.savePlayer();
+    if (this.isLoaded) {
+      this.animate(ctx);
     }
+  }
 
-    savePlayer(){
-        localStorage.setItem("player", JSON.stringify({
-            lvl: this.lvl,
-            exp: this.exp,
-            requiredExp: this.requiredExp
-        }));
-    }
-
-    loaded(){
-        this.isLoaded = true;
-    }
-
-    draw(ctx, t){
-        if(!this.time){
-            this.time = t;
-        }
-
-        const now = t - this.time;
-        if(now > this.fpsTime){
-            this.time = t;
-            this.curFrame += 1;
-            if(this.curFrame == this.totalFrame){
-                this.curFrame = 0;
-            }
-        }
-        if(this.isLoaded){
-            this.animate(ctx);
-        }
-    }
-
-    animate(ctx){
-        ctx.drawImage(
-            this.img,
-            this.imgWidth * this.curFrame,
-            0,
-            this.imgWidth,
-            this.imgHeight,
-            0,
-            0,
-            this.imgWidth * 2,
-            this.imgHeight * 2
-        );
-    }
+  animate(ctx) {
+    ctx.drawImage(
+      this.img,
+      this.imgWidth * this.curFrame,
+      0,
+      this.imgWidth,
+      this.imgHeight,
+      0,
+      0,
+      this.imgWidth,
+      this.imgHeight
+    );
+  }
 }
--- view.js
+++ view.js
@@ -1,42 +1,41 @@
-export class View{
-    constructor(player){
-        this.btnAddTodo = document.getElementById("btnAddTodo");
-        this.inputAddTodo = document.getElementById("inputAddTodo");
-        this.todoList = document.getElementById("todoList");
+export class View {
+  constructor(player) {
+    this.btnAddTodo = document.getElementById("btnAddTodo");
+    this.inputAddTodo = document.getElementById("inputAddTodo");
+    this.todoList = document.getElementById("todoList");
 
-        this.canvas = document.getElementById("playerCanvas");
-        this.ctx = this.canvas.getContext('2d');
-        this.canvas.width = 64;
-        this.canvas.height = 64;
+    this.canvas = document.getElementById("playerCanvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = 64;
+    this.canvas.height = 96;
 
-        this.player = player;
-    }
+    this.player = player;
+  }
 
-    render(todos){
-        this.clearList();
-        todos.forEach((item) => {
-            this.todoList.innerHTML += this.getTodoTemplate(item);
-        })        
-        this.inputAddTodo.value = "";
-    }
+  render(todos) {
+    this.clearList();
+    todos.forEach((item) => {
+      this.todoList.innerHTML += this.getTodoTemplate(item);
+    });
+    this.inputAddTodo.value = "";
+  }
 
-    renderPlayer(player){
-        const lvl = document.getElementById("lvl");
-        const exp = document.getElementById("exp");
-        const requiredExp = document.getElementById("requiredExp");
+  renderPlayer(player) {
+    const lvl = document.getElementById("lvl");
+    const exp = document.getElementById("exp");
+    const requiredExp = document.getElementById("requiredExp");
 
-        lvl.innerText = player.lvl;
-        exp.innerText = player.exp;
-        requiredExp.innerText = player.requiredExp;
-    }
+    lvl.innerText = player.lvl;
+    exp.innerText = player.exp;
+    requiredExp.innerText = player.requiredExp;
+  }
 
-    clearList(){
-        this.todoList.innerHTML = "";
-    }
+  clearList() {
+    this.todoList.innerHTML = "";
+  }
 
-    getTodoTemplate(item){
-        
-        return `
+  getTodoTemplate(item) {
+    return `
         <li id=${item.id}>
             <span class="todoText">${item.todo}</span>
             <button class="btnDeleteTodo"> x </button>
@@ -44,55 +43,55 @@ export class View{
             <img class="monsterImg" src="/src/${item.monster}.png"></img>
         </li>
         `;
-    }
+  }
 
-    addTodoHandler(handler){       
-        this.btnAddTodo.addEventListener("click", (event)=>{
-            event.preventDefault;
-            if(this.inputAddTodo.value){
-                handler(this.inputAddTodo.value);
-            }
-        });       
-        this.inputAddTodo.addEventListener("keyup", (event)=>{
-            if(event.keyCode == 13){
-                event.preventDefault;
-                if(this.inputAddTodo.value){
-                    handler(this.inputAddTodo.value);
-                }
-            }
-        });
-    }
+  addTodoHandler(handler) {
+    this.btnAddTodo.addEventListener("click", (event) => {
+      event.preventDefault;
+      if (this.inputAddTodo.value) {
+        handler(this.inputAddTodo.value);
+      }
+    });
+    this.inputAddTodo.addEventListener("keyup", (event) => {
+      if (event.keyCode == 13) {
+        event.preventDefault;
+        if (this.inputAddTodo.value) {
+          handler(this.inputAddTodo.value);
+        }
+      }
+    });
+  }
 
-    deleteTodoHandler(handler){       
-        this.todoList.addEventListener("click", (event)=>{
-            event.preventDefault;
-            const target = event.target;
-            if(target.classList[0] === 'btnDeleteTodo'){
-                handler(target.parentNode.id);
-                this.todoList.removeChild(target.parentNode);
-            }
-        });
-    }
+  deleteTodoHandler(handler) {
+    this.todoList.addEventListener("click", (event) => {
+      event.preventDefault;
+      const target = event.target;
+      if (target.classList[0] === "btnDeleteTodo") {
+        handler(target.parentNode.id);
+        this.todoList.removeChild(target.parentNode);
+      }
+    });
+  }
 
-    completeTodoHandler(handler, player){       
-        this.todoList.addEventListener("click", (event)=>{
-            event.preventDefault;
-            const target = event.target;
-            if(target.classList[0] === 'btnCompleteTodo'){
-                handler(target.parentNode.id);
-                this.todoList.removeChild(target.parentNode);
-                this.renderPlayer(player);
-            }
-        });
-    }    
+  completeTodoHandler(handler, player) {
+    this.todoList.addEventListener("click", (event) => {
+      event.preventDefault;
+      const target = event.target;
+      if (target.classList[0] === "btnCompleteTodo") {
+        handler(target.parentNode.id);
+        this.todoList.removeChild(target.parentNode);
+        this.renderPlayer(player);
+      }
+    });
+  }
 
-    draw(){        
-        requestAnimationFrame(this.animate.bind(this));
-    }
+  draw() {
+    requestAnimationFrame(this.animate.bind(this));
+  }
 
-    animate(t){
-        requestAnimationFrame(this.animate.bind(this));
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.player.draw(this.ctx, t);
-    }
+  animate(t) {
+    requestAnimationFrame(this.animate.bind(this));
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.player.draw(this.ctx, t);
+  }
 }
